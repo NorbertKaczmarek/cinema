ce370526929bb3481bf02f842134e4e62422d1c9
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-10-08 15:00:31 +0200

    init

---
+++ cinema.api.tests.unit/BasicTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cinema.api.tests.unit;
+
+public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Get_HelloWorld_Endpoint_ReturnsHelloWorld()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/api/");
+
+        response.EnsureSuccessStatusCode();
+        var responseString = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello World!", responseString);
+    }
+
+    [Fact]
+    public async Task Swagger_Endpoint_ReturnsOk()
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync("/swagger");
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
---
+++ cinema.api/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapGet("/api/", () => "Hello World!");
+app.MapGet("/api/2/", () => "Hello World! 2");
+app.MapGet("/api/3/", () => "Hello World! 3");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+public partial class Program { } // used by Unit Testing
