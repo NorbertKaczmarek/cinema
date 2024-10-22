a309c0bc74e6f82310ab0e77f38a6ca278e8db79
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-10-22 15:05:23 +0200

    added controllers

---
+++ cinema.api/Controllers/OrdersController.cs
@@ -0,0 +1,51 @@
+﻿using cinema.context.Entities;
+using cinema.context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cinema.api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrdersController : ControllerBase
+{
+    private readonly CinemaDbContext _context;
+
+    public OrdersController(CinemaDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Order> Get()
+    {
+        return _context.Orders.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public Order Get(Guid id)
+    {
+        return getById(id);
+    }
+
+    private Order getById(Guid id)
+    {
+        return _context.Orders.FirstOrDefault(m => m.Id == id)!;
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        // TODO
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(Guid id)
+    {
+        var order = getById(id);
+        if (order == null) return;
+
+        _context.Orders.Remove(order);
+        _context.SaveChanges();
+    }
+}
---
+++ cinema.api/Controllers/ScreeningsController.cs
@@ -0,0 +1,50 @@
+﻿using cinema.context.Entities;
+using cinema.context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cinema.api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ScreeningsController : ControllerBase
+{
+    private readonly CinemaDbContext _context;
+
+    public ScreeningsController(CinemaDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IEnumerable<Screening> Get()
+    {
+        return _context.Screenings.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public Screening Get(Guid id)
+    {
+        return getById(id);
+    }
+
+    private Screening getById(Guid id)
+    {
+        return _context.Screenings.FirstOrDefault(m => m.Id == id)!;
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        // TODO
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(Guid id)
+    {
+        var screening = getById(id);
+        if (screening == null) return;
+
+        _context.Screenings.Remove(screening);
+        _context.SaveChanges();
+    }
+}
