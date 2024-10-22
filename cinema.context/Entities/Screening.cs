858dfc270e441c3718b8f223fca6dcf476ba3323
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-10-22 14:56:13 +0200

    refactor

--- cinema.context/Entities/Screening.cs
+++ cinema.context/Entities/Screening.cs
@@ -5,5 +5,6 @@ public class Screening
     public Guid Id { get; set; }
     public DateTimeOffset StartDateTime { get; set; }
     public DateTimeOffset EndDateTime { get; set; }
-    public Guid MovieId { get; set; }
+    public Guid? MovieId { get; set; }
+    public virtual Movie? Movie { get; set; }
 }
