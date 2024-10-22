763421eb46c9b9a960555fc7d20adbef1711565c
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-10-22 16:41:59 +0200

    update test project

---
+++ cinema.tests/BasicTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace cinema.tests;
+
+public class BasicTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Theory]
+    [InlineData("/swagger")]
+    [InlineData("/api/categories")]
+    [InlineData("/api/Movies")]
+    [InlineData("/api/Orders")]
+    [InlineData("/api/Screenings")]
+    [InlineData("/api/Seats")]
+    public async Task Get_Endpoints_ReturnsSuccessStatusCode(string url)
+    {
+        var client = _factory.CreateClient();
+
+        var response = await client.GetAsync(url);
+
+        response.EnsureSuccessStatusCode();
+    }
+}
---
+++ cinema.tests/CategoriesControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace cinema.tests;
+
+public class CategoriesControllerTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public CategoriesControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
