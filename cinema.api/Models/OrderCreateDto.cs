92dd587aadb91d2039070dc7ead08f177126b848
Norbert Kaczmarek <kontakt@norbertkaczmarek.pl>
2024-12-13 17:23:45 +0100

    logic changes

--- cinema.api/Models/OrderCreateDto.cs
+++ cinema.api/Models/OrderCreateDto.cs
@@ -2,8 +2,8 @@
 
 public class OrderCreateDto
 {
-    public required string Email { get; set; }
-    public required string PhoneNumber { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public string Status { get; set; }
     public Guid ScreeningId { get; set; }
     public List<Guid> SeatIds { get; set; } = new List<Guid>();
