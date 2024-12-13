e8fd7f851f6bd143f7f80f1c5085525794a2d9b1
Sebastian Maczynski <sebastian.maczynski@britenet.eu>
2024-12-13 16:03:05 +0100

    Add datetimepicker component

--- cinema.ui/src/components/Cells/OrderStatusCell/OrderStatusCell.tsx
+++ cinema.ui/src/components/Cells/OrderStatusCell/OrderStatusCell.tsx
@@ -7,16 +7,19 @@ interface Props {
 const statusMap = {
   [OrderStatus.READY]: {
     title: 'Opłacony',
+    className: 'text-success font-bold',
   },
   [OrderStatus.PENDING]: {
     title: 'Oczekujący',
+    className: 'text-warning font-bold',
   },
   [OrderStatus.CANCELLED]: {
     title: 'Anulowany',
+    className: 'text-destructive font-bold',
   },
 } as const;
 
 export const OrderStatusCell = ({ status }: Props) => {
   // @TODO classname WITH color
-  return <span>{statusMap[status].title ?? ''}</span>;
+  return <span className={statusMap[status].className}>{statusMap[status].title ?? ''}</span>;
 };
